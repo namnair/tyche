93f31295978c546de44492db73e7827ae97d5dcc
namnair <93312156+namnair@users.noreply.github.com>
2022-12-18 10:11:49 +0530

    card and flip and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 import { useState } from "react";
-import ReactCardFlip from "./Flipcard";
+
+import ReactCardFlip from "react-card-flip";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const [card, setCard] = useState({ text: "", summary: "" });
-
   const [allCards, setAllCards] = useState([]);
 
   console.log(allCards);
@@ -38,33 +38,44 @@ function App() {
     const result = await response.json();
     return result;
   }
+  const flashcards = document.querySelectorAll(".flashcard");
 
+  flashcards.forEach((flashcard) => {
+    flashcard.addEventListener("click", () => {
+      flashcard.classList.toggle("flipped");
+    });
+  });
   return (
     <div className="App">
       <div className="input">
         <textarea
+          className="txtarea"
           type="text"
           name="long"
           value={card.text}
-          onclick="$(this).val('');"
           onChange={(e) => {
             e.preventDefault();
             setCard({ text: e.target.value });
           }}
         ></textarea>
-        <button onClick={dowhenclicked}>Submit</button>
+
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={dowhenclicked}
+        >
+          Submit
+        </button>
       </div>
-      <div className="container">
-        <div className="row">
-          <div className="col_mid">
-            <div class="card">
-              <div class="card-body">
-                <div className="output">
-                  {allCards.map(function display(card) {
-                    return <div className="display-card">{card.summary}</div>;
-                  })}
-                </div>
-              </div>
+      <div className="flashcard-container">
+        <div className="flashcard">
+          <div className="flashcard-front">learn smarter</div>
+          <div className="flashcard-back">
+            {" "}
+            <div className="output">
+              {allCards.map(function display(card) {
+                return <div className="display-card">{card.summary}</div>;
+              })}
             </div>
           </div>
         </div>
